26769ff0185372387c64ffc0de1d9e892aa6f94e
Smriti Sharma <154870779+Smriti-sh@users.noreply.github.com>
2024-09-14 14:03:00 +0530

    llm api

--- UI/src/app/generator/generator.component.ts
+++ UI/src/app/generator/generator.component.ts
@@ -5,6 +5,9 @@ import { Routes } from '@angular/router';
 import { Router } from '@angular/router';
 import { ActivatedRoute } from '@angular/router';
 import { FormControl } from '@angular/forms';
+import { FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-generator',
@@ -12,6 +15,13 @@ import { FormControl } from '@angular/forms';
   styleUrl: './generator.component.css'
 })
 export class GeneratorComponent implements OnInit{
+
+queryForm: FormGroup = new FormGroup({
+    query: new FormControl('', [Validators.required]),
+    // num: new FormControl('', [Validators.required]),
+    // bookName: new FormControl('', [Validators.required]),
+  });
+  isApiCalling = false;
 
 
   formats: Format[]=[
@@ -25,8 +35,40 @@ export class GeneratorComponent implements OnInit{
     }
   ]
 
-  constructor(private router: Router){}
+  constructor(
+    private router: Router,
+    private http: HttpClient,
+    private toastr: ToastrService
+  ){}
 
   ngOnInit(): void {}
 
+  resetForm(): void {
+    this.queryForm.reset();
+  }
+
+  onSubmit(): void {
+    if (this.queryForm.valid) {
+      const queryData = new FormData();
+      queryData.append('query', this.queryForm.get('query')?.value);
+      // queryData.append('queNum', this.queryForm.get('num')?.value);
+      // queyData.append('bookName', this.queryForm.get('bookName')?.value);
+      this.isApiCalling = true;
+      this.http.post('http://localhost:3000/api/llm/query', queryData).subscribe(
+        res => {
+          console.log('Query entered succesfully', res);
+          this.resetForm();
+        },
+        error => {
+          console.error('Error processing query', error);
+          this.toastr.error('There was an error!');
+        },
+        () => this.isApiCalling = false
+      );
+     }else {
+      this.toastr.error('Form is invalid or no file selected.');
+    }
 }
+
+}
+
