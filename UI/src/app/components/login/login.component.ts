938aceabd6fe0ad51a75cadd204491eb1c396b24
Vaibhav Chaudhary <vaibhavchaudhary14107@gmail.com>
2024-09-25 21:06:03 +0530

    Update router

--- UI/src/app/components/login/login.component.ts
+++ UI/src/app/components/login/login.component.ts
@@ -5,7 +5,6 @@ import { AbstractControl, FormControl, ValidationErrors, ValidatorFn } from '@an
 import { FormGroup, Validators } from '@angular/forms';
 import { NameValidator } from '../../core/validators/name.validator';
 import { ToastrService } from 'ngx-toastr';
-import { error, log } from 'console';
 import { AuthService } from '../../core/services/auth.service';
 import { UsersDataSource } from '../../services/Users.dataSource';
 
@@ -16,24 +15,24 @@ import { UsersDataSource } from '../../services/Users.dataSource';
 })
 export class LoginComponent {
 
-  panel:string = 'login';
-  isLoggedIn:boolean=false;
+  panel: string = 'login';
+  isLoggedIn: boolean = false;
 
   //for login
   SubmitForm: FormGroup = new FormGroup({
-    email: new FormControl('', [Validators.required,NameValidator.noExtraSpaces]),
-    confirmPassword: new FormControl('',[Validators.required])
+    email: new FormControl('', [Validators.required, NameValidator.noExtraSpaces]),
+    confirmPassword: new FormControl('', [Validators.required])
   });
 
   //for registration
   NewForm: FormGroup = new FormGroup({
-    username: new FormControl('',[Validators.required]),
-    email: new FormControl('', [Validators.required,NameValidator.noExtraSpaces]),
-    password: new FormControl('',[Validators.required]),
-    confirmPassword: new FormControl('',[Validators.required]),
-  },{validators: this.customPasswordMatching.bind(this) });
+    username: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, NameValidator.noExtraSpaces]),
+    password: new FormControl('', [Validators.required]),
+    confirmPassword: new FormControl('', [Validators.required]),
+  }, { validators: this.customPasswordMatching.bind(this) });
 
-  response:any;
+  response: any;
   passwordMismatch: ValidatorFn | ValidatorFn[] | null | undefined;
   static isLoggedIn: any;
 
@@ -43,74 +42,75 @@ export class LoginComponent {
     private authService: AuthService,
     private router: Router,
     private $UsersDataSource: UsersDataSource
-  ){}
+  ) {
+    if (this.authService.isLoggedIn()) {
+      this.router.navigate(['books'])
+    }
+  }
 
-  customPasswordMatching(NewForm:AbstractControl): { [key: string]: boolean } | null  {
+  customPasswordMatching(NewForm: AbstractControl): { [key: string]: boolean } | null {
     const password = NewForm.get('password')?.value;
     const confirmPassword = NewForm.get('confirmPassword')?.value;
     // return password === confirmPassword ? null : { passwordMismatch: true };
     return password === confirmPassword ? null : { passwordMismatchError: true };
   }
 
-  switchType(event:string){
+  switchType(event: string) {
     this.panel = event;
   }
 
   resetForm(): void {
     this.SubmitForm.reset();
   }
-  
+
   onSubmitLogin = async () => {
-    try{
-      if (this.SubmitForm.valid ) {
-  
-      const loginData = {
-        email: this.SubmitForm.value.email, // Replace this with the actual form control names
-        password: this.SubmitForm.value.confirmPassword 
-      };
-  
-      const resp = await this.authService.login(loginData.email,loginData.password).toPromise();
-      this.resetForm();
-      console.log(resp, "Resp");
-      if(resp && resp.token){
-        this.authService.setToken(resp.token);
-        this.$UsersDataSource.setLoggedIn(true);
+    try {
+      if (this.SubmitForm.valid) {
+
+        const loginData = {
+          email: this.SubmitForm.value.email, // Replace this with the actual form control names
+          password: this.SubmitForm.value.confirmPassword
+        };
+
+        const resp = await this.authService.login(loginData.email, loginData.password).toPromise();
         this.resetForm();
-       this.router.navigate(['/books']);
+        if (resp && resp.token) {
+          this.authService.setToken(resp.token);
+          this.$UsersDataSource.setLoggedIn(true);
+          this.resetForm();
+          this.router.navigate(['/books']);
+        }
+      } else {
+        this.toastr.error('Form is invalid');
       }
-    } else{
-      this.toastr.error('Form is invalid');
-    }
-    } catch(err){
+    } catch (err) {
       console.log(err);
     }
   }
- 
+
   // TODO SignUp - username,email, password, confirmPassword
-  onSubmitNew = async () =>{
-    try{
+  onSubmitNew = async () => {
+    try {
       if (this.NewForm.valid) {
         const signInData = {
           username: this.NewForm.value.username,
           email: this.NewForm.value.email,
           password: this.NewForm.value.password
         }
-        const resp = await this.authService.register(signInData.username,signInData.email,signInData.password).toPromise();
-        console.log("resp",resp);
-        
+        const resp = await this.authService.register(signInData.username, signInData.email, signInData.password).toPromise();
+
         if (resp && resp.token) {
           this.authService.setToken(resp.token);
           this.isLoggedIn = false;
-          console.log("Token", this.authService.getToken());
           this.resetForm();
           this.router.navigate(['/books']);
-        }else{
+        } else {
           this.toastr.error("Form is invalid")
         }
       }
-    }catch(err){
+    } catch (err) {
       console.log(err);
     }
   }
-  
+
 }
