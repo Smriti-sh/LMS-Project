e58ee62b1c71d34b3ab5fc2b0dd4e20bb0493d66
Smriti-sh <smritisapr2001@gmail.com>
2026-02-06 11:20:41 +0530

    UI URL ADDED

--- api/index.js
+++ api/index.js
@@ -101,3 +101,7 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+app.use(cors({
+  origin: "https://lms-project-1-an4a.onrender.com"
+}));
